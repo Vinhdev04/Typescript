0f38fa43da9ee3431afb36856e106ddee1b398a7
Vinh Dev <vinh@example.com>
2026-04-10 02:43:30 +0700

    update code lab02

---
+++ Labs/lab02/index.ts
@@ -0,0 +1,14 @@
+
+/* 
+    function sumJS(a,b){
+        return a+b;
+    }
+    console.log(sumJS(2,"Vinh"));
+*/
+
+function sumTS(a:number, b:number):number{
+    return a + b;
+}
+
+// console.log(sumTS(2,`Vinh`));
+console.log(sumTS(2,3));
