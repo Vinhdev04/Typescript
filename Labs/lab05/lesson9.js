b70801782cc87a66d99283e40e38e7d88c540566
Vinh Dev <vinh@example.com>
2026-04-14 02:23:19 +0700

    Type String in ts

---
+++ Labs/lab05/lesson9.js
@@ -0,0 +1,7 @@
+/*
+    String Type
+*/
+
+let myName = "mynameisvinh";
+let myAge = 21;
+console.log(`Hi! ${myName}, I'm ${myAge} years old.`);
---
+++ Labs/lab05/lesson9.ts
@@ -0,0 +1,7 @@
+/*
+    String Type
+*/
+
+let myNames: string = "Vinh \"Macao\"";
+let myOld: number = 20;
+console.log(`Hi! ${myNames}, I'm ${myOld} years old.`);
