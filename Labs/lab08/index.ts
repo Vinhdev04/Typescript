4ad52466e8c40f46f0f7f40ad28ec3802dfa4173
Vinh Dev <vinh@example.com>
2026-06-02 20:36:16 +0700

    kieu du lieu trong typescrypt

---
+++ Labs/lab08/index.ts
@@ -0,0 +1,18 @@
+// number
+let count: number = 100;
+console.log('Count', count);
+
+// boolean
+let isPushed: boolean = true;
+(isPushed) ? console.log('Pushed...') : console.log('Not Pushed...');
+
+// string
+let str: string = "this is string";
+console.log(str);
+
+// object
+const listUser: string[] = ['user1', 'user2', 'user3'];
+
+listUser.forEach((item: string) => {
+    console.log(item);
+});
