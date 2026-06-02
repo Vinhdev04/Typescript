4ebb39a0032ed15fa555340c377fd22fa5002c7a
Vinh Dev <vinh@example.com>
2026-06-02 21:48:51 +0700

    void in ts

---
+++ Labs/lab11/void.ts
@@ -0,0 +1,13 @@
+// void
+
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log("sum is: ", sum(10, 20));
+
+// void -> ko return gì cả
+const printSum = (a: number, b: number): void => {
+  console.log(a + b);
+};
+printSum(10, 20);
