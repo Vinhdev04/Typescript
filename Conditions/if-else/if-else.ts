b486427491d6395d23257b8fa4a7ac89a6a7a9fc
Vinh Dev <vinh@example.com>
2026-06-05 22:53:37 +0700

    if-else in ts

---
+++ Conditions/if-else/if-else.ts
@@ -0,0 +1,13 @@
+/**
+ * condition: if-else
+ */
+
+let isActived: boolean = true;
+console.log("Before: ", isActived);
+let resultX: string = isActived ? "Actived here!>.." : "Not actived!?...";
+console.log("resultX: ", resultX);
+
+let address: string = "Ha Tinh";
+address === "Ha Tinh"
+  ? console.log("Here Ha Tinh!>...")
+  : console.log("Not here!");
